150caf077c708f8515c602483abf2d752663f22f
Namkat Cedrick j <namkatcedrickjumtock3@gmail.com>
2022-07-23 00:03:44 +0100

    edited header section

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
-import '../styles/globals.css'
+import { Loader } from "../components/Loader";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Loader />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
