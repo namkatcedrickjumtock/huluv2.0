964becce040f5a9d4da97c23e0d5c95fdec44ff3
Namkat Cedrick <namkatcedrickjumtock3@gmail.com>
2022-08-11 08:56:53 +0100

    response from api but not accurate

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ["image.tmdb.org", "links.papareact.com"],
+    domains: ["image.tmdb.org", "links.papareact.com","image.shutterstock.com"],
   },
 };
 
